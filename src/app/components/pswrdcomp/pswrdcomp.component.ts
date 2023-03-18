da501b2d25c7729b1c100fa4e44aed8b386a2a24
youssef-El-Hajji <youssef1995.7@gmail.com>
2023-03-19 00:40:26 +0100

    'commit'

--- src/app/components/pswrdcomp/pswrdcomp.component.ts
+++ src/app/components/pswrdcomp/pswrdcomp.component.ts
@@ -5,19 +5,15 @@ import { ServicegeneratorService } from 'src/app/service/servicegenerator.servic
 @Component({
   selector: 'app-pswrdcomp',
   templateUrl: './pswrdcomp.component.html',
-  styleUrls: ['./pswrdcomp.component.scss']
+  styleUrls: ['./pswrdcomp.component.scss'],
 })
-export class PswrdcompComponent implements OnInit{
-val:String = '---';
-  constructor(private service:ServicegeneratorService){
+export class PswrdcompComponent implements OnInit {
+  val: String = '---';
+  constructor(private service: ServicegeneratorService) {}
+
+  ngOnInit() {
+    this.service.dataObeservable.subscribe((password) => {
+      this.val = password;
+    });
   }
-  
-  ngOnInit(){
-    this.service.dataObeservable.subscribe(
-      (password)=>{
-        this.val = password;
-      }
-    );
-    }
-  
 }
--- src/app/service/servicegenerator.service.ts
+++ src/app/service/servicegenerator.service.ts
@@ -3,59 +3,56 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServicegeneratorService {
-  private valueTmp='';
-  private length='';
-  private data={};
-  private password='';
+  private valueTmp = '';
+  private length = '';
+  private data = {};
+  private password = '';
   private elements = '';
-  private checks=[];
-  private upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  private lower = "abcdefghijklmnopqrstuvwxyz";
-  private numbers = "0123456789";
-  private symbols = "!\"#$%&'*+,./:;=?@\\^`|~_-[]{}()<>";
+  private checks = [];
+  private upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  private lower = 'abcdefghijklmnopqrstuvwxyz';
+  private numbers = '0123456789';
+  private symbols = '!"#$%&\'*+,./:;=?@\\^`|~_-[]{}()<>';
   ///c1?:Boolean,c2?:Boolean,c3?:Boolean,c4?:Boolean
-  constructor() { 
-    
-  }
+  constructor() {}
   dataSubject = new Subject<String>();
   dataObeservable = this.dataSubject.asObservable();
 
-set_data_checkedBox(c1:Boolean,c2:Boolean,c3:Boolean,c4:Boolean){
-  this.valueTmp='';
-  if(c1){
-    this.valueTmp+=this.lower;
+  set_data_checkedBox(c1: Boolean, c2: Boolean, c3: Boolean, c4: Boolean) {
+    this.valueTmp = '';
+    if (c1) {
+      this.valueTmp += this.lower;
+    }
+    if (c2) {
+      this.valueTmp += this.upper;
+    }
+    if (c3) {
+      this.valueTmp += this.numbers;
+    }
+    if (c4) {
+      this.valueTmp += this.symbols;
+    }
+    console.log('value is', this.valueTmp);
   }
-  if(c2){
-    this.valueTmp+=this.upper;
-  }
-  if(c3){
-    this.valueTmp+=this.numbers;
-  }
-  if(c4){
-    this.valueTmp+=this.symbols;
-  }
-  console.log('value is',this.valueTmp);
-} 
 
-setValue(val:any){
-  this.length = val;
-}
-generatePassword(){
-  this.elements = this.valueTmp;
-  this.password ='';
-  console.log('From gen',this.elements);
-  console.log('From gen',this.length);
-
-  console.log(parseInt(this.length));
-  for (let i = 0; i <= parseInt(this.length) -1; i++) {
-    let randomNumber = Math.floor(Math.random() * this.elements.length);
-    this.password += this.elements.substring(randomNumber, randomNumber + 1);
+  setValue(val: any) {
+    this.length = val;
   }
-this.dataSubject.next(this.password);
-return this.password;
-}
+  generatePassword() {
+    this.elements = this.valueTmp;
+    this.password = '';
+    console.log('From gen', this.elements);
+    console.log('From gen', this.length);
 
+    console.log(parseInt(this.length));
+    for (let i = 0; i <= parseInt(this.length) - 1; i++) {
+      let randomNumber = Math.floor(Math.random() * this.elements.length);
+      this.password += this.elements.substring(randomNumber, randomNumber + 1);
+    }
+    this.dataSubject.next(this.password);
+    return this.password;
+  }
 }
